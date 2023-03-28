a0e5651f7133a7f97022db7157b8a8ea08e2ab31
xyntechx <xyntechx@gmail.com>
2023-03-28 08:30:34 +0800

    ✨ Check if logged in

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
-import type { Component } from "solid-js";
+import { Component, createSignal } from "solid-js";
 import { lazy } from "solid-js";
 import { Routes, Route, Navigate } from "@solidjs/router";
+import { pb } from "./utils/pocketbase";
 import styles from "./App.module.css";
 
+const Auth = lazy(() => import("./routes/auth"));
 const Dashboard = lazy(() => import("./routes/dashboard"));
 const Create = lazy(() => import("./routes/create"));
 
 const App: Component = () => {
+    const [user, setUser] = createSignal(pb.authStore.model);
+
+    pb.authStore.onChange(() => setUser(pb.authStore.model));
+
     return (
         <div class={styles.app}>
             <Routes>
-                <Route path="/" element={<Navigate href="/dashboard" />} />
+                <Route
+                    path="/"
+                    element={
+                        <Navigate href={user() ? "/dashboard" : "/auth"} />
+                    }
+                />
+                <Route path="/auth" component={Auth} />
                 <Route path="/dashboard" component={Dashboard} />
                 <Route path="/create" component={Create} />
             </Routes>
