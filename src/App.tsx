477f82fbbf1ffe1663f129dbe5b45d03e47f0869
xyntechx <xyntechx@gmail.com>
2023-03-28 11:18:45 +0800

    ✨ Implement user context

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { Component, createSignal } from "solid-js";
+import { Component } from "solid-js";
 import { lazy } from "solid-js";
 import { Routes, Route, Navigate } from "@solidjs/router";
-import { pb } from "./utils/pocketbase";
+import { UserProvider } from "./contexts/UserContext";
 import styles from "./App.module.css";
 
 const Auth = lazy(() => import("./routes/auth"));
@@ -9,23 +9,16 @@ const Dashboard = lazy(() => import("./routes/dashboard"));
 const Create = lazy(() => import("./routes/create"));
 
 const App: Component = () => {
-    const [user, setUser] = createSignal(pb.authStore.model);
-
-    pb.authStore.onChange(() => setUser(pb.authStore.model));
-
     return (
         <div class={styles.app}>
-            <Routes>
-                <Route
-                    path="/"
-                    element={
-                        <Navigate href={user() ? "/dashboard" : "/auth"} />
-                    }
-                />
-                <Route path="/auth" component={Auth} />
-                <Route path="/dashboard" component={Dashboard} />
-                <Route path="/create" component={Create} />
-            </Routes>
+            <UserProvider>
+                <Routes>
+                    <Route path="/" element={<Navigate href="/auth" />} />
+                    <Route path="/auth" component={Auth} />
+                    <Route path="/dashboard" component={Dashboard} />
+                    <Route path="/create" component={Create} />
+                </Routes>
+            </UserProvider>
         </div>
     );
 };
---
+++ src/contexts/UserContext.tsx
@@ -0,0 +1,31 @@
+import { createContext, createSignal, JSX, useContext } from "solid-js";
+import { pb } from "../utils/pocketbase";
+
+interface Props {
+    children: JSX.Element;
+}
+
+const UserContext = createContext();
+
+export const UserProvider = (props: Props) => {
+    const [user, setUser] = createSignal(pb.authStore.model);
+    pb.authStore.onChange(() => setUser(pb.authStore.model));
+
+    const [isVerified, setIsVerified] = createSignal(
+        localStorage.getItem("isVerified")
+            ? JSON.parse(localStorage.getItem("isVerified")!)
+            : false
+    );
+
+    return (
+        <UserContext.Provider
+            value={{ user, setUser, isVerified, setIsVerified }}
+        >
+            {props.children}
+        </UserContext.Provider>
+    );
+};
+
+export const useUser = (): any => {
+    return useContext(UserContext);
+};
