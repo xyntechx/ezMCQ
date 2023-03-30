d1875d95deb3c86d28ea8e4c5fd3d57ace444b06
xyntechx <xyntechx@gmail.com>
2023-03-30 09:45:21 +0800

    ✨ Add /paper/:id and 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Component } from "solid-js";
 import { lazy } from "solid-js";
-import { Routes, Route, Navigate } from "@solidjs/router";
+import { Routes, Route, Navigate, A } from "@solidjs/router";
 import { UserProvider } from "./contexts/UserContext";
 import styles from "./App.module.css";
 
 const Auth = lazy(() => import("./routes/auth"));
 const Dashboard = lazy(() => import("./routes/dashboard"));
 const Create = lazy(() => import("./routes/create"));
+const Paper = lazy(() => import("./routes/paper"));
 
 const App: Component = () => {
     return (
@@ -17,6 +18,23 @@ const App: Component = () => {
                     <Route path="/auth" component={Auth} />
                     <Route path="/dashboard" component={Dashboard} />
                     <Route path="/create" component={Create} />
+                    <Route
+                        path="/paper"
+                        element={<Navigate href="/dashboard" />}
+                    />
+                    <Route path="/paper/:id" component={Paper} />
+                    <Route
+                        path="/*"
+                        element={
+                            <p>
+                                Oops! This page doesn't exist.{" "}
+                                <A href="/" class={styles.link}>
+                                    Back to home
+                                </A>
+                                .
+                            </p>
+                        }
+                    />
                 </Routes>
             </UserProvider>
         </div>
